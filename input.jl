652fe0c61982eb2e68141666750e01c0e0370192
WuJC <jcwu@hqu.edu.cn>
2023-09-08 16:11:59 +0800

    20230908

--- input.jl
+++ input.jl
@@ -1,7 +1,7 @@
 
-function import_tri3(filename1::String,filename2::String)
-    elms,~ = ApproxOperator.importmsh(filename1)
-    elms_p,~ = ApproxOperator.importmsh(filename2)
+function import_mf_tri3(filename1::String,filename2::String)
+    elms,nds = ApproxOperator.importmsh(filename1)
+    elms_p,nds_p = ApproxOperator.importmsh(filename2)
     nₚ = length(elms["Ω"][1].x)
 
     x = elms["Ω"][1].x
@@ -13,6 +13,9 @@ function import_tri3(filename1::String,filename2::String)
     # scheme = ApproxOperator.quadraturerule(s)
 
     d = zeros(nₚ)
+    𝗠 = zeros(n𝒑)
+    ∂𝗠∂x = zeros(n𝒑)
+    ∂𝗠∂y = zeros(n𝒑)
     elements = Dict{String,Vector{ApproxOperator.AbstractElement}}()
 
     f_Ω = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13)
@@ -27,30 +30,30 @@ function import_tri3(filename1::String,filename2::String)
         :𝝭=>:𝑠,
         :∂𝝭∂x=>:𝑠,
         :∂𝝭∂y=>:𝑠,
-        :𝗠=>:𝑠,
-        :∂𝗠∂x=>:𝑠,
-        :∂𝗠∂y=>:𝑠
+        :𝗠=>(:𝐶,𝗠),
+        :∂𝗠∂x=>(:𝐶,∂𝗠∂x),
+        :∂𝗠∂y=>(:𝐶,∂𝗠∂y)
     )
     push!(f_Ωᵖ,
         :d=>(:𝐼,d),
         :𝝭=>:𝑠,
         :∂𝝭∂x=>:𝑠,
         :∂𝝭∂y=>:𝑠,
-        :𝗠=>:𝑠,
-        :∂𝗠∂x=>:𝑠,
-        :∂𝗠∂y=>:𝑠
+        :𝗠=>(:𝐶,𝗠),
+        :∂𝗠∂x=>(:𝐶,∂𝗠∂x),
+        :∂𝗠∂y=>(:𝐶,∂𝗠∂y)
     )
     push!(f_Γᵍ,
         :𝝭=>:𝑠,
-        :𝗠=>:𝑠
+        :𝗠=>(:𝐶,𝗠),
     )
     if haskey(elms,"Γᵗ")
         f_Γᵗ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Seg2},:SegGI2)
         elements["Γᵗ"] = f_Γᵗ(elms["Γᵗ"])
         push!(f_Γᵗ,
             :𝝭=>:𝑠,
-            :𝗠=>:𝑠
+            :𝗠=>(:𝐶,𝗠),
         )
     end
-    return elements, d, elms_p
+    return elements, nodes, nodes_p
 end
