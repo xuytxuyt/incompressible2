283b72b492eb6156012499cd37c5365d6357917f
WuJC <jcwu@hqu.edu.cn>
2023-09-14 15:21:00 +0800

    20230914

--- input.jl
+++ input.jl
@@ -3,15 +3,20 @@ function import_mf_tri3(filename1::String,filename2::String)
     elms,~= ApproxOperator.importmsh(filename1)
     elms_p,~ = ApproxOperator.importmsh(filename2)
     nₚ = length(elms["Ω"][1].x)
+    nᵖ = length(elms_p["Ω"][1].x)
 
     x = elms["Ω"][1].x
     y = elms["Ω"][1].y
     z = elms["Ω"][1].z
+    xᵖ = elms_p["Ω"][1].x
+    yᵖ = elms_p["Ω"][1].y
+    zᵖ = elms_p["Ω"][1].z
 
-    nodes = Node{(:𝐼,),1}[]
-   
     data = Dict([:x=>(1,x),:y=>(1,y),:z=>(1,z)])
     nodes = [Node{(:𝐼,),1}((i,),data) for i in 1:nₚ]
+    data_p = Dict([:x=>(1,xᵖ),:y=>(1,yᵖ),:z=>(1,zᵖ)])
+    nodes_p = [Node{(:𝐼,),1}((i,),data_p) for i in 1:nᵖ]
+
     sp = ApproxOperator.RegularGrid(x,y,z,n=1,γ=2)
     parameters = (:Linear2D,:□,:CubicSpline)
     n𝒑 = 21
@@ -21,9 +26,9 @@ function import_mf_tri3(filename1::String,filename2::String)
     ∂𝗠∂y = zeros(n𝒑)
     elements = Dict{String,Vector{ApproxOperator.AbstractElement}}()
 
-    f_Ω = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13)
-    f_Ωᵖ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13)
-    f_Γᵍ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Seg2},:SegGI2)
+    f_Ω = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13,data)
+    f_Ωᵖ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13,data_p)
+    f_Γᵍ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Seg2},:SegGI2,data)
 
     elements["Ω"] = f_Ω(elms["Ω"])
     elements["Ωᵖ"] = f_Ωᵖ(elms_p["Ω"])
@@ -49,14 +54,15 @@ function import_mf_tri3(filename1::String,filename2::String)
         :𝗠=>(:𝐶,𝗠),
     )
     if haskey(elms,"Γᵗ")
-        f_Γᵗ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Seg2},:SegGI2)
+        f_Γᵗ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Seg2},:SegGI2,data)
         elements["Γᵗ"] = f_Γᵗ(elms["Γᵗ"])
         push!(f_Γᵗ,
             :𝝭=>:𝑠,
             :𝗠=>(:𝐶,𝗠),
         )
     end
-    return elements, nodes
+
+    return elements, nodes, nodes_p
 end
 
 function import_fem_tri3(filename1::String,filename2::String)
--- patch_test.jl
+++ patch_test.jl
@@ -6,14 +6,13 @@ elements,nodes = import_mf_tri3("./msh/cantilever_2.msh","./msh/cantilever_2.msh
 
 nₚ = length(nodes)
 
-
 s = 2.5*12/2*ones(nₚ)
 
 push!(nodes,:s₁=>s,:s₂=>s,:s₃=>s)
 
-set𝝭!.(elements["Ω"])
-set∇𝝭!.(elements["Ω"])
-set𝝭!.(elements["Γᵍ"])
+set𝝭!(elements["Ω"])
+set∇𝝭!(elements["Ω"])
+set𝝭!(elements["Γᵍ"])
 
 E = 3e6
 ν=0.3
