6d97ac7a8ed370aca989ce7d17ea86f4894a1157
xuytxuyt <xyt5517@163.com>
2023-10-07 15:01:58 +0800

    20231007

---
+++ eigen.jl
@@ -0,0 +1,58 @@
+using Revise, ApproxOperator, LinearAlgebra, Printf, XLSX
+ndiv=8
+include("input.jl")
+
+# elements,nodes,nodes_p = import_fem_tri3_GI1("./msh/cantilever_"*string(ndiv)*".msh","./msh/cantilever_"*string(ndiv)*".msh")
+elements,nodes,nodes_p = import_quad_GI1("./msh/cantilever_quad_"*string(ndiv)*".msh","./msh/cantilever_quad_"*string(ndiv)*".msh")
+
+
+nₚ = length(nodes)
+
+set𝝭!(elements["Ω"])
+set∇𝝭!(elements["Ω"])
+set𝝭!(elements["Ωᵛ"])
+set∇𝝭!(elements["Ωᵛ"])
+set𝝭!(elements["Γᵍ"])
+
+P = 1000
+ Ē = 3e6
+ν̄ = 0.49999
+# ν̄ = 0.3
+E = Ē/(1.0-ν̄^2)
+ν = ν̄/(1.0-ν̄)
+L = 12
+D = 12
+I = D^3/12
+EI = E*I
+I = D^3/12
+EI = E*I
+
+prescribe!(elements["Γᵍ"],:g₁=>(x,y,z)->-P*y/6/EI*((6*L-3*x)*x + (2+ν)*(y^2-D^2/4)))
+prescribe!(elements["Γᵍ"],:g₂=>(x,y,z)->P/6/EI*(3*ν*y^2*(L-x) + (4+5*ν)*D^2*x/4 + (3*L-x)*x^2))
+prescribe!(elements["Γᵍ"],:n₁₁=>(x,y,z)->1.0)
+prescribe!(elements["Γᵍ"],:n₁₂=>(x,y,z)->0.0)
+prescribe!(elements["Γᵍ"],:n₂₂=>(x,y,z)->1.0)
+
+ops = [
+       Operator{:∫∫εᵢⱼσᵢⱼdxdy}(:E=>E,:ν=>ν),
+       Operator{:∫vᵢtᵢds}(),
+       Operator{:∫vᵢgᵢds}(:α=>1e9*E),
+       Operator{:Hₑ_PlaneStress}(:E=>E,:ν=>ν)
+]
+opsᵛ = [
+    Operator{:∫∫εᵛᵢⱼσᵛᵢⱼdxdy}(:E=>Ē,:ν=>ν̄ )
+]
+opsᵈ = [
+    Operator{:∫∫εᵈᵢⱼσᵈᵢⱼdxdy}(:E=>Ē,:ν=>ν̄ )
+]
+kᵛ = zeros(2*nₚ,2*nₚ)
+kᵈ = zeros(2*nₚ,2*nₚ)
+kᵍ = zeros(2*nₚ,2*nₚ) 
+f = zeros(2*nₚ)
+
+
+opsᵛ[1](elements["Ωᵛ"],kᵛ)
+# opsᵛ[1](elements["Ω"],kᵛ)
+opsᵈ[1](elements["Ω"],kᵈ)
+ops[3](elements["Γᵍ"],kᵍ,f)
+v = eigvals(kᵛ,kᵈ+kᵍ)
--- input.jl
+++ input.jl
@@ -155,6 +155,88 @@ function import_fem_tri3(filename1::String,filename2::String)
     return elements, nodes, nodes_p
 end
 
+function import_fem_tri3_GI1(filename1::String,filename2::String)
+    elms,~ = ApproxOperator.importmsh(filename1)
+    elms_p,~ = ApproxOperator.importmsh(filename2)
+    nₚ = length(elms["Ω"][1].x)
+    nᵖ = length(elms_p["Ω"][1].x)
+
+    x = elms["Ω"][1].x
+    y = elms["Ω"][1].y
+    z = elms["Ω"][1].z
+    xᵖ = elms_p["Ω"][1].x
+    yᵖ = elms_p["Ω"][1].y
+    zᵖ = elms_p["Ω"][1].z
+   
+    data = Dict([:x=>(1,x),:y=>(1,y),:z=>(1,z)])
+    nodes = [Node{(:𝐼,),1}((i,),data) for i in 1:nₚ]
+    data_p = Dict([:x=>(1,xᵖ),:y=>(1,yᵖ),:z=>(1,zᵖ)])
+    nodes_p = [Node{(:𝐼,),1}((i,),data_p) for i in 1:nᵖ]
+
+    sp = ApproxOperator.RegularGrid(x,y,z,n=1,γ=2)
+    parameters = (:Linear2D,:□,:CubicSpline)
+    n𝒑 = 21
+
+
+    𝗠 = zeros(n𝒑)
+    ∂𝗠∂x = zeros(n𝒑)
+    ∂𝗠∂y = zeros(n𝒑)
+    elements = Dict{String,Vector{ApproxOperator.AbstractElement}}()
+
+    f_Ω = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Tri3},:TriGI3,data)
+    f_Ωᵛ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Tri3},:TriGI1,data)
+    f_Ωᵖ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13,data_p)
+    f_Γᵍ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Seg2},:SegGI2,data)
+
+    elements["Ω"] = f_Ω(elms["Ω"])
+    elements["Ωᵛ"] = f_Ωᵛ(elms["Ω"])
+    elements["Ωᵖ"] = f_Ωᵖ(elms_p["Ω"])
+    elements["Γᵍ"] = f_Γᵍ(elms["Γᵍ"])
+    push!(f_Ω,
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+    )
+    push!(f_Ωᵛ,
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+    )
+    push!(f_Ωᵖ,
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+        :𝗠=>(:𝐶,𝗠),
+        :∂𝗠∂x=>(:𝐶,∂𝗠∂x),
+        :∂𝗠∂y=>(:𝐶,∂𝗠∂y)
+    )
+    push!(f_Γᵍ,
+        :𝝭=>:𝑠,
+        
+    )
+    if haskey(elms,"Γᵗ")
+        f_Γᵗ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Seg2},:SegGI2,data)
+        elements["Γᵗ"] = f_Γᵗ(elms["Γᵗ"])
+        n₁ = zeros(length(elms["Γᵗ"]))
+        n₂ = zeros(length(elms["Γᵗ"]))
+        push!(f_Γᵗ,
+            :𝝭=>:𝑠,
+            :n₁=>(:𝐶,n₁),
+            :n₂=>(:𝐶,n₂),
+        )
+        for ap in elements["Γᵗ"]
+            nd₁,nd₂ = ap.𝓒
+            x₁ = nd₁.x
+            x₂ = nd₂.x
+            y₁ = nd₁.y
+            y₂ = nd₂.y
+            𝐿 = ((x₁-x₂)^2+(y₁-y₂)^2)^0.5
+            ap.n₁ = (y₂-y₁)/𝐿
+            ap.n₂ = (x₁-x₂)/𝐿
+        end
+    end
+    return elements, nodes, nodes_p
+end
 function import_fem_tri3_direct(filename1::String,filename2::String)
     elms,~ = ApproxOperator.importmsh(filename1)
     elms_p,~ = ApproxOperator.importmsh(filename2)
@@ -274,6 +356,76 @@ function import_quad(filename1::String,filename2::String)
     elements["Ωᵖ"] = f_Ωᵖ(elms_p["Ω"])
     elements["Γᵍ"] = f_Γᵍ(elms["Γᵍ"])
     push!(f_Ω,
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+    )
+    push!(f_Ωᵖ,
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+    )
+    push!(f_Γᵍ,
+        :𝝭=>:𝑠,
+    )
+    if haskey(elms,"Γᵗ")
+        f_Γᵗ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Seg2},:SegGI2,data)
+        elements["Γᵗ"] = f_Γᵗ(elms["Γᵗ"])
+        n₁ = zeros(length(elms["Γᵗ"]))
+        n₂ = zeros(length(elms["Γᵗ"]))
+        push!(f_Γᵗ,
+            :𝝭=>:𝑠,
+            :n₁=>(:𝐶,n₁),
+            :n₂=>(:𝐶,n₂),
+        )
+        for ap in elements["Γᵗ"]
+            nd₁,nd₂ = ap.𝓒
+            x₁ = nd₁.x
+            x₂ = nd₂.x
+            y₁ = nd₁.y
+            y₂ = nd₂.y
+            𝐿 = ((x₁-x₂)^2+(y₁-y₂)^2)^0.5
+            ap.n₁ = (y₂-y₁)/𝐿
+             ap.n₂ = (x₁-x₂)/𝐿
+        end
+    end
+    return elements, nodes, nodes_p
+end
+
+function import_quad_GI1(filename1::String,filename2::String)
+    elms,~ = ApproxOperator.importmsh(filename1)
+    elms_p,~ = ApproxOperator.importmsh(filename2)
+    nₚ = length(elms["Ω"][1].x)
+    nᵖ = length(elms_p["Ω"][1].x)
+    x = elms["Ω"][1].x
+    y = elms["Ω"][1].y
+    z = elms["Ω"][1].z
+    xᵖ = elms_p["Ω"][1].x
+    yᵖ = elms_p["Ω"][1].y
+    zᵖ = elms_p["Ω"][1].z
+
+    data = Dict([:x=>(1,x),:y=>(1,y),:z=>(1,z)])
+    nodes = [Node{(:𝐼,),1}((i,),data) for i in 1:nₚ]
+    data_p = Dict([:x=>(1,xᵖ),:y=>(1,yᵖ),:z=>(1,zᵖ)])
+    nodes_p = [Node{(:𝐼,),1}((i,),data_p) for i in 1:nᵖ]
+
+    elements = Dict{String,Vector{ApproxOperator.AbstractElement}}()
+
+    f_Ω = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Quad},:QuadGI4,data)
+    f_Ωᵛ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Quad},:QuadGI1,data)
+    f_Ωᵖ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Quad},:QuadGI4,data_p)
+    f_Γᵍ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Seg2},:SegGI2,data)
+
+    elements["Ω"] = f_Ω(elms["Ω"])
+    elements["Ωᵛ"] = f_Ωᵛ(elms["Ω"])
+    elements["Ωᵖ"] = f_Ωᵖ(elms_p["Ω"])
+    elements["Γᵍ"] = f_Γᵍ(elms["Γᵍ"])
+    push!(f_Ω,
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+    )
+        push!(f_Ωᵛ,
         :𝝭=>:𝑠,
         :∂𝝭∂x=>:𝑠,
         :∂𝝭∂y=>:𝑠,
